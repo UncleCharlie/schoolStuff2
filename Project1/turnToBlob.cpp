bf4652c460deeeb3e4cde882c027a682562c689d
UncleCharlie <guthkaroly1@gmail.com>
2018-11-26 13:03:42 +0100

    threshold javítva

--- Project1/turnToBlob.cpp
+++ Project1/turnToBlob.cpp
@@ -2,8 +2,10 @@
 
 void turnToBlob(const Mat& src, Mat dest, double kuszobArany) {
 
-	kuszobol(src, dest, kuszobArany);
+	//kuszobol(src, dest, kuszobArany);
+	threshold(src, dest,250, 255, THRESH_BINARY);
 	Mat kernel = cv::getStructuringElement(MORPH_RECT, Size(5, 5));
+	morphologyEx(dest, dest, MORPH_CLOSE, kernel);
 	erode(dest, dest, kernel);
 
 }
